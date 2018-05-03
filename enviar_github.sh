7e7c332a8fe87a2ef84d2c0cea569ec2eb4750d6
Vinicius Hashirama <vinnihashirama@gmail.com>
2018-05-02 21:26:14 -0300

    OOoooopa acabamos de clonar pelo SHEEEEELL

---
+++ enviar_github.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Vinicius Hashirama"
+git config --global user.email vinnihashirama@gmail.com
+git add *
+git commit -m "OOoooopa acabamos de clonar pelo SHEEEEELL"
+git push
